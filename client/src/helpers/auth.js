458566c19fc9014374d3cb3d0a4cfc8b075cb853
andela-oakinrele <oluwasinmisola.akinrele@andela.com>
2017-03-31 19:11:49 +0100

    remove localhost

--- client/src/helpers/auth.js
+++ client/src/helpers/auth.js
@@ -1,7 +1,7 @@
 import request from './request';
 
 export const login = (credentials, cb) => {
-  request('http://localhost:5000/users/login', 'post', credentials, (err, res) => {
+  request('/users/login', 'post', credentials, (err, res) => {
     if (err) {
       cb(err);
     } else {
