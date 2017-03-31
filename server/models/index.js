b9530a39ba2ef128f16be326300f874e1f9eb1a1
andela-oakinrele <oluwasinmisola.akinrele@andela.com>
2017-03-31 19:05:06 +0100

    config var

--- server/models/index.js
+++ server/models/index.js
@@ -4,6 +4,7 @@ const Sequelize = require('sequelize');
 const config = require('./../config/config');
 
 const sequelize = new Sequelize(config.database, config.username, config.password, {
+  host: config.host,
   dialect: 'postgres'
 });
 
