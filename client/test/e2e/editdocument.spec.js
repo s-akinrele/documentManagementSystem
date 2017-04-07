fc6e995228aa6a6479f8e5a51fd483867de70ddb
andela-oakinrele <oluwasinmisola.akinrele@andela.com>
2017-04-07 16:22:26 +0100

    remove pause

--- client/test/e2e/editdocument.spec.js
+++ client/test/e2e/editdocument.spec.js
@@ -9,7 +9,6 @@ module.exports = {
      .setValue('input[type=email]', 'akinrelesimi@gmail.com')
      .setValue('input[type=password]', 'password')
      .click('button[type=submit]')
-     .pause(1000)
      .assert.urlContains('dashboard')
      .waitForElementVisible('body')
      .assert.elementPresent('#cardpanel')
@@ -18,7 +17,6 @@ module.exports = {
      .waitForElementVisible('div[id="view"]', 10000)
      .click('button[id="edit"]')
      .waitForElementVisible('div[class="modal-content"')
-     .pause(5000)
      .execute(`tinyMCE.activeEditor.setContent('${faker.lorem.words()}')`)
      .click('button[id="save"]')
      .end();
--- client/test/e2e/signup.spec.js
+++ client/test/e2e/signup.spec.js
@@ -11,14 +11,12 @@ module.exports = {
         .click('.signup')
         .moveToElement('.signup', 0, 0)
         .mouseButtonClick(0)
-        .pause(10000)
         .setValue('input[name=username]', faker.internet.userName())
         .setValue('input[name=firstname]', faker.name.firstName())
         .setValue('input[name=lastname]', faker.name.lastName())
         .setValue('input[id="em"]', faker.internet.email())
         .setValue('input[id="pd"]', 'password')
         .click('button[id="sub"]')
-        .pause(10000)
         .assert.urlContains('dashboard')
         .end();
   }
